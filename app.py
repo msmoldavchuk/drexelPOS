de238abb0b0045f476690feac4a8e95bb1530f51
MasonJarCat <MasonMcClinticC@gmail.com>
2023-03-02 09:03:06 -0500

    some ajustment to the webend with js and flask

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import requests
+from flask import json
+from flask import jsonify
 app = Flask(__name__)
 
 
@@ -21,9 +23,51 @@ def plan():
 @app.route('/POS')
 def pos():
     return render_template('courseinput.html')
+#reroutes to POS pages
+@app.route('/POS/CS')
+def poscs():
+    if request.method == 'POST':
+        return render_template('POS/CS.html')
+#API endpoints
+@app.route('/api/POS', methods=['GET', 'POST'])
+def api_pos():
+    if request.method == 'POST':
+        if request.form.get('major'):
+            return redirect(url_for('POS/CS'))
+@app.route('/api/POS/CS', methods=['GET', 'POST'])
+def api_poscs():
+    #this takes the form data from the CS POS page and runs it through the API
+    #returns the data as a json object
+    #method()
+    return jsonify({'place': 'holder'})
+
+
+#test routes
+#does not use for anything except testing
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
+
+
+@app.route('/test/data', methods=['GET', 'POST'])
+def testfn2():
+    if request.method == 'POST':
+        
+        print(request.get_data(as_text=True))
+        message = request.get_data(as_text=True)
+        return jsonify(message)
+
 @app.route('/temp', methods=['GET', 'POST'])
 def temp():
     if request.method == 'POST':
         if request.form.get('name'):
-            return render_template('temp.html', name="hello")
+            return render_template('temp.html', name="hi")
     return render_template('temp.html')
+            
